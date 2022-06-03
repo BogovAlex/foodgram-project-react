7efc2bd2253e20812307ab931a6d6d05b88467dd
Bogov Alexey <bogov-alexey@yandex.ru>
2022-06-03 20:04:29 +0300

    work

--- backend/foodgram_project/app/views.py
+++ backend/foodgram_project/app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404
-from django.db.models import Sum
+# from django.db.models import Sum
 from django.http import HttpResponse
-from django.db.models import QuerySet
+# from django.db.models import QuerySet
 from django.conf import settings
 from django_filters.rest_framework import DjangoFilterBackend
 
