948125d5c4493ceecfbdddc3e55d609c062d7212
Bogov Alexey <bogov-alexey@yandex.ru>
2022-06-03 17:42:16 +0300

    work

--- backend/foodgram_project/foodgram_project/settings.py
+++ backend/foodgram_project/foodgram_project/settings.py
@@ -32,7 +32,12 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ['backend', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = (
+    'backend',
+    '127.0.0.1',
+    'localhost',
+    'foodgram.viewdns.net',
+)
 
 
 # Application definition
